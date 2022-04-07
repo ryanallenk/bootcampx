05f6fc91f39f308ec81e8794444a2b9425ea1e59
ryanallenk <ryanallenk@gmail.com>
2022-04-06 22:51:57 -0400

    add connections using node postgres

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,17 @@
+const { Pool } = require('pg');
+
+const pool = new Pool();
+
+pool.query(`SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+ORDER BY teacher;
+`)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+}).catch(err => console.error('query error', err.stack));
